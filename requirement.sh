0e3540d4484606fbbf9973ef74535c28b12d7fa4
rootbakar <43517550+rootbakar@users.noreply.github.com>
2024-06-09 23:53:25 +0700

    Update requirement.sh

--- requirement.sh
+++ requirement.sh
@@ -3,6 +3,7 @@
 # Install Python 3.8 if not installed
 if ! command -v python3.8 &>/dev/null; then
     echo "Python 3.8 is not installed. Installing..."
+    sudo apt update -y
     sudo apt install python3.8 -y
     wget https://bootstrap.pypa.io/get-pip.py
     python3.8 get-pip.py
