cbda94d41c4a01151951ba62a9a1f382ad4904ef
rootbakar <43517550+rootbakar@users.noreply.github.com>
2024-06-09 23:59:55 +0700

    Update requirement-mac.sh

--- requirement-mac.sh
+++ requirement-mac.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Install Python 3.8 if not installed
-if ! command -v python3.8 &>/dev/null; then
-    echo "Python 3.8 is not installed. Installing..."
-    brew install python@3.8 #if not installed
-    brew reinstall python@3.8 #if installed
-    brew cleanup python@3.8
+if ! command -v python3 &>/dev/null; then
+    echo "Python 3 is not installed. Installing..."
+    brew install python@3 #if not installed
+    brew reinstall python@3 #if installed
+    brew cleanup python@3
     wget https://bootstrap.pypa.io/get-pip.py
-    python3.8 get-pip.py
+    python3 get-pip.py
 fi
 
 # Install Python dependencies
-pip3.8 install -r requirements.txt
+python3 -m pip install -r requirements.txt
 
 # Copy lucek.py to /usr/local/bin
 chmod +x lucek.py
