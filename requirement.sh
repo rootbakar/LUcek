ba281a6aba92b32502ecd22b5629b6da6e7020a3
rootbakar <43517550+rootbakar@users.noreply.github.com>
2024-06-09 11:04:11 +0700

    Update requirement.sh

--- requirement.sh
+++ requirement.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Install Python 3 if not installed
-if ! command -v python3 &>/dev/null; then
-    echo "Python 3 is not installed. Installing..."
-    sudo apt install python3 -y
+# Install Python 3.8 if not installed
+if ! command -v python3.8 &>/dev/null; then
+    echo "Python 3.8 is not installed. Installing..."
+    sudo apt install python3.8 -y
 fi
 
 # Install Python dependencies
