4855b9abcde71a687c3616057cb3848634ca70d9
rootbakar <43517550+rootbakar@users.noreply.github.com>
2024-11-23 01:34:06 +0700

    Update requirement-mac.sh

--- requirement-mac.sh
+++ requirement-mac.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
 
-# Install Python 3 if not installed
-if ! command -v python3 &>/dev/null; then
-    echo "Python 3 is not installed. Installing..."
-    brew install python@3 #if not installed
-    brew reinstall python@3 #if installed
-    brew cleanup python@3
-    wget https://bootstrap.pypa.io/get-pip.py
-    python3 get-pip.py
-fi
-
 # Install Python dependencies
-python3 -m pip install -r requirements.txt
-python3 -m pip install -r requirements.txt --break-system-packages
+python3.11 -m pip install . --break-system-packages
 
 # Copy lucek.py to /usr/local/bin
 chmod +x lucek.py
