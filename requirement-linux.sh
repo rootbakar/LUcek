9eaaae41fb3233cd4807e2eb2f4d0e7a4fe6e103
rootbakar <43517550+rootbakar@users.noreply.github.com>
2024-11-23 01:36:43 +0700

    Update requirement-linux.sh

--- requirement-linux.sh
+++ requirement-linux.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
-# Install Python 3 if not installed
-if ! command -v python3 &>/dev/null; then
-    echo "Python 3 is not installed. Installing..."
-    sudo apt update -y
-    sudo apt install python3 -y
-    sudo apt install python3-pip
-fi
-
 # Install Python dependencies
-python3 -m pip install -r requirements.txt
-python3 -m pip install -r requirements.txt --break-system-packages
+python3.11 -m pip install -r requirements.txt --break-system-packages
 
 # Copy lucek.py to /usr/local/bin
 sudo chmod +x lucek.py
